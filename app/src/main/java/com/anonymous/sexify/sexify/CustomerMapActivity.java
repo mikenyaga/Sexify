bbb1226d07ba0a2d91550f31420df558947d9e58
mikenyaga <mikeantonynyaga@gmail.com>
2018-03-15 16:10:53 +0300

    MVP

--- app/src/main/java/com/anonymous/sexify/sexify/CustomerMapActivity.java
+++ app/src/main/java/com/anonymous/sexify/sexify/CustomerMapActivity.java
@@ -1,15 +1,13 @@
 package com.anonymous.sexify.sexify;
 
-import android.*;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
-import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -205,8 +203,7 @@ public class CustomerMapActivity extends FragmentActivity implements OnMapReadyC
         mylastLocation = location;
         LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
         mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(-4));
-
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
 
     }
 
